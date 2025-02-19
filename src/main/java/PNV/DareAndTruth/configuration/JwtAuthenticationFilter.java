d1914830c287880a02b2147d437d5561a221d147
Nguyễn Lâm Nhật <zznhatphilong@gmail.com>
2025-02-19 10:59:46 +0700

    add ignore filter public route

--- src/main/java/PNV/DareAndTruth/configuration/JwtAuthenticationFilter.java
+++ src/main/java/PNV/DareAndTruth/configuration/JwtAuthenticationFilter.java
@@ -32,6 +32,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
+        String requestPath = request.getServletPath();
+
+        // Ignore filter if it is public route
+        if (requestPath.equals("/auth/sign-up") ||
+                requestPath.equals("/auth/sign-in") ||
+                requestPath.equals("/auth/refresh-token")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
