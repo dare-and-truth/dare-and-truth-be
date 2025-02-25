65aaa086da438fe647584f0f07232601f7b77dd5
Mai74D <nguyenmaioc0@gmail.com>
2025-02-25 10:37:39 +0700

    create calendar entity

---
+++ src/main/java/PNV/DareAndTruth/entity/Calendar.java
@@ -0,0 +1,40 @@
+package PNV.DareAndTruth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "calendars")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Calendar extends BaseEntity{
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(name = "title", nullable = false)
+    String title;
+
+    @Column(name = "start_date", nullable = false)
+    LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    LocalDate endDate;
+
+    @Column(name = "start_time", nullable = false)
+    LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    LocalTime endTime;
+
+    @Column(name = "repeat_type", nullable = false)
+    int repeatType; //1-Does not repeat , 2-Daily, 3-weekly, 4-monthly
+}
