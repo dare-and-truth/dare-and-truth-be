1128ab096b6271b9097228460be3d75707221f7b
Mai74D <nguyenmaioc0@gmail.com>
2025-03-19 11:16:07 +0700

    run format

--- src/main/java/PNV/DareAndTruth/dto/request/comment/CreateCommentRequest.java
+++ src/main/java/PNV/DareAndTruth/dto/request/comment/CreateCommentRequest.java
@@ -1,6 +1,5 @@
 package PNV.DareAndTruth.dto.request.comment;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.UUID;
--- src/main/java/PNV/DareAndTruth/dto/request/comment/UpdateCommentRequest.java
+++ src/main/java/PNV/DareAndTruth/dto/request/comment/UpdateCommentRequest.java
@@ -1,7 +1,5 @@
 package PNV.DareAndTruth.dto.request.comment;
 
-import jakarta.validation.constraints.NotBlank;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
